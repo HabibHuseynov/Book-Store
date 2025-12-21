61e61cf8a2b00d939b36f916c80ac6f0d5b0ef77
Habib Huseynov <habibhuseynov@Habibs-MacBook-Pro.local>
2025-12-21 19:47:17 +0400

    added orders client and security

---
+++ webapp/src/main/java/com/example/webapp/config/ApplicationProperties.java
@@ -0,0 +1,6 @@
+package com.example.webapp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "bookstore")
+public record ApplicationProperties(String apiGatewayUrl) {}
